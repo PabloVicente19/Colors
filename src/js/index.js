5b8befab876aab6f8302bfb09e6ecc9211604bb1
Pablo Vicente <pablovicente19@hotmail.com>
2022-12-02 18:07:24 -0300

    queda estilizart

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,21 @@
 
-const createColor = () =>{
-    
-
-        const simbolos = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
-          // simbolos = "0123456789ABCDEF";
-          let color = "#";
-      
-          for(var i = 0; i < 6; i++){
-              color = color + simbolos[Math.floor(Math.random() * 16)];
-          }
-        console.log(color)
-        content.style.background = color;
+const generateColor = () =>{
+    const numsHexa = ['1','2','3','4','5','6','7',"8","9","A","B","C","D","E","F"]
+    let color= '#';
+      for(let i = 0; i < 6; i ++){
+        color = color + numsHexa[Math.floor(Math.random() * 15)];
       }
-  
-
-
-const generateColor = (e) => {
-    e.preventDefault();
-    createColor()
-    e.reset()
-    // printColor();
-    // // cardColor()
-    // // exampleColor()
+      content.style.background = color;
 }
 
+const showColor = (e) => {
+    e.preventDefault();
+    generateColor()
+    form.reset()
 
+}
 
-
-
-
-
-
-
-
-
-
-const showColor = () => { form.addEventListener('click', generateColor) }
 const init = () => {
-    showColor()
+  form.addEventListener('click', showColor)
 }
 init()
